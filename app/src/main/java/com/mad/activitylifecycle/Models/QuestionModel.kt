6e1a293bb0c7b22b7e816d3c6c47eb1909a06199
LewWaiKit <kitwai2013@gmail.com>
2021-08-05 17:19:57 +0800

    Initial commit

---
+++ app/src/main/java/com/mad/activitylifecycle/Models/QuestionModel.kt
@@ -0,0 +1,34 @@
+package com.mad.activitylifecycle.Models
+
+import androidx.lifecycle.ViewModel
+
+class QuestionModel: ViewModel() {
+    var name: String = ""
+    var no1: Int =0
+    var no2: Int= 0
+    var answer: Int = 0
+
+    init{
+        generateQuestion()
+    }
+    fun getanswer():Int{
+        return no1+no2
+    }
+
+    fun generateQuestion(): String{
+        no1 = (Math.random()*100).toInt()
+        no2 = (Math.random()*100).toInt()
+
+        return "${no1} + ${no2} =?"
+    }
+    fun getQuestion(): String {
+        return "${no1} + ${no2} =?"
+    }
+    companion object{
+        private var instance : QuestionModel? = null
+        fun getInstance() =
+            instance ?: synchronized(QuestionModel::class.java){
+                instance ?: QuestionModel().also {instance = it}
+            }
+    }
+}
